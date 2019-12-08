223e7c3559a7894ff041e9c37998e62234368b4d
Refri Rifwan <refririfwan@gmail.com>
2019-12-08 21:50:06 +0700

    user & admin page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,16 +1,16 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'My Profile';
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
         $this->load->view('templates/topbar');
-        $this->load->view('user/index');
+        $this->load->view('admin/index');
         $this->load->view('templates/footer');
     }
 }
---
+++ application/views/admin/index.php
@@ -0,0 +1,7 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+</div>
+<!-- /.container-fluid -->
