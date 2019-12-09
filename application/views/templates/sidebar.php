cb3c3f8ceade38ba24512d7b9889efadee08d914
Refri Rifwan <refririfwan@gmail.com>
2019-12-09 09:27:46 +0700

    management menu

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,34 +11,48 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+    <!-- Query Menu -->
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                    FROM  `user_menu` JOIN `user_access_menu`
+                    ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC";
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <!-- Looping Menu -->
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
+    <?php foreach ($menu as $m) : ?>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            <?= $m['menu'] ?>
+        </div>
 
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user"></i>
-            <span>My Profile</span></a>
-    </li>
+        <!-- Siapkan sub-menu sesuai menu-->
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <?php
+            $querySubMenu = "SELECT *
+                    FROM  `user_sub_menu` JOIN `user_menu`
+                    ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                    WHERE `user_sub_menu`.`menu_id` = {$m['id']}
+                    AND `user_sub_menu`.`is_active`";
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
+
+        <?php foreach ($subMenu as $sm) : ?>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                    <i class="<?= $sm['icon'] ?>"></i>
+                    <span><?= $sm['title'] ?></span></a>
+            </li>
+        <?php endforeach ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+    <?php endforeach ?>
 
     <li class="nav-item">
         <a class="nav-link" href="<?= base_url('auth/logout') ?>">
