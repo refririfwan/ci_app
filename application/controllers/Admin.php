d10351caede2e596c5a316c7ca2b19cac64db363
Refri Rifwan <refririfwan@gmail.com>
2019-12-10 22:07:55 +0700

    access management

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,4 +18,63 @@ class Admin extends CI_Controller
         $this->load->view('admin/index');
         $this->load->view('templates/footer');
     }
+
+    public function role()
+    {
+        $data['title'] = 'Role';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['role'] = $this->db->get('user_role')->result_array();
+        $this->form_validation->set_rules('role', 'Role', 'required');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar');
+            $this->load->view('templates/topbar');
+            $this->load->view('admin/role');
+            $this->load->view('templates/footer');
+        } else {
+            $this->db->insert('user_role', [
+                'role' => $this->input->post('role')
+            ]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> New Role Added! </div>');
+            redirect('admin/role/');
+        }
+    }
+
+    public function deleteRole($id)
+    {
+        $this->db->delete('user_role', ['id' => $id]);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Role Has Been Deleted! </div>');
+        redirect('admin/role/');
+    }
+
+    public function roleAccess($role_id)
+    {
+        $data['title'] = 'Role Access';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['role'] = $this->db->get_where('user_role', ['id' => $role_id])->row_array();
+        $this->db->where('id != ', 1);
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('admin/role-access');
+        $this->load->view('templates/footer');
+    }
+
+    public function changeAccess()
+    {
+        $menu_id = $this->input->post('menuId');
+        $role_id = $this->input->post('roleId');
+
+        $data = ['role_id' => $role_id, 'menu_id' => $menu_id];
+
+        $result = $this->db->get_where('user_access_menu', $data);
+
+        if ($result->num_rows() < 1) {
+            $this->db->insert('user_access_menu', $data);
+        } else {
+            $this->db->delete('user_access_menu', $data);
+        }
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Access Changed!</div>');
+    }
 }
--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -17,4 +17,13 @@ function is_logged_in()
             redirect('auth/blocked');
         }
     }
+
+    function check_access($role_id, $menu_id)
+    {
+        $ci = get_instance();
+        $result = $ci->db->get_where('user_access_menu', ['role_id' => $role_id, 'menu_id' => $menu_id]);
+        if ($result->num_rows() > 0) {
+            return "checked='checked'";
+        }
+    }
 }
