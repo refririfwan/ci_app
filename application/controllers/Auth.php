1950dd08877a8677bba927c0274fdd3c0221ce49
Refri Rifwan <refririfwan@gmail.com>
2019-12-12 19:30:34 +0700

    forgot password

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -106,18 +106,21 @@ class Auth extends CI_Controller
         $config = array();
         $config['protocol'] = 'smtp';
         $config['smtp_host'] = 'ssl://smtp.googlemail.com';
-        $config['smtp_user'] = 'email';
-        $config['smtp_pass'] = 'password';
+        $config['smtp_user'] = 'your email';
+        $config['smtp_pass'] = 'your password';
         $config['smtp_port'] = 465;
         $config['mailtype'] = 'html';
         $config['charset'] = 'utf-8';
         $this->email->initialize($config);
         $this->email->set_newline("\r\n");
-        $this->email->from('email', 'CI_APP');
+        $this->email->from('your email', 'CI_APP');
         $this->email->to($this->input->post('email'));
         if ($type == 'verify') {
             $this->email->subject('Account Verification');
             $this->email->message('Click this link to verify your account : <a href="' . base_url('auth/verify?email=' . $this->input->post('email')) . '&token=' . urlencode($token) . '">Activate</a>');
+        } else if ($type == 'forgot') {
+            $this->email->subject('Reset Password');
+            $this->email->message('Click this link to reset your password : <a href="' . base_url('auth/resetpassword?email=' . $this->input->post('email')) . '&token=' . urlencode($token) . '">Reset Password</a>');
         }
         if ($this->email->send()) {
             return true;
@@ -172,5 +175,79 @@ class Auth extends CI_Controller
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/blocked');
         $this->load->view('templates/auth_footer');
+    }
+
+    public function forgotPassword()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Forgot Passowrd';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/forgot-password');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $email = $this->input->post('email');
+            $user = $this->db->get_where('user', ['email' => $email, 'is_active' => 1])->row_array();
+            if ($user) {
+                $token = base64_encode(random_bytes(32));
+                $user_token = [
+                    'email' => $email,
+                    'token' => $token,
+                    'date_created' => time()
+                ];
+                $this->db->insert('user_token', $user_token);
+                $this->_sendEmail($token, 'forgot');
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Please check your email for reset password! </div>');
+                redirect('auth/forgotpassword');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered or activated! </div>');
+                redirect('auth/forgotpassword');
+            }
+        }
+    }
+
+    public function resetPassword()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+            if ($user_token) {
+                $this->session->set_userdata('reset_email', $email);
+                $this->changePassword();
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Reset password failed! Wrong token! </div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Reset password failed! Wrong email! </div>');
+            redirect('auth');
+        }
+    }
+
+    public function changePassword()
+    {
+        if (!$this->session->userdata('reset_email')) {
+            redirect('auth');
+        }
+        $this->form_validation->set_rules('password1', 'Password', 'trim|required|min_length[3]|matches[password2]');
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'trim|required|min_length[3]|matches[password1]');
+        $data['title'] = 'Change Passowrd';
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/change-password');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $password = password_hash($this->input->post('password1'), PASSWORD_DEFAULT);
+            $email = $this->session->userdata('reset_email');
+            $this->db->set('password', $password);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+            $this->session->unset_userdata('reset_email');
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Password has been changed! Please Login.</div>');
+            redirect('auth');
+        }
     }
 }
--- application/views/auth/forgot-password.php
+++ application/views/auth/forgot-password.php
@@ -12,28 +12,21 @@
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Forgot your password ?</h1>
                                     </div>
                                     <?= $this->session->flashdata('message'); ?>
-                                    <form class="user" method="POST" action="<?= base_url('auth/index') ?>">
+                                    <form class="user" method="POST" action="<?= base_url('auth/forgotpassword') ?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="email" value="<?= set_value('email') ?>">
                                             <?= form_error('email', '<small class="text-danger pl-4">', '</small>') ?>
                                         </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
-                                            <?= form_error('password', '<small class="text-danger pl-4">', '</small>') ?>
-                                        </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                            Reset Password
                                         </button>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="<?= base_url('auth/forgotpassword') ?>">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="<?= base_url('auth/registration') ?>">Create an Account!</a>
+                                        <a class="small" href="<?= base_url('auth/index') ?>">Back to login</a>
                                     </div>
                                 </div>
                             </div>
